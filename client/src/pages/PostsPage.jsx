2bb76ba7bd7d9abbc48ef9fa40425b26c7264444
Dev <devextraemail@gmail.com>
2024-05-05 12:28:57 -0400

    Connected posts page to backend

--- client/src/pages/PostsPage.jsx
+++ client/src/pages/PostsPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Post, AddPost } from "../components";
+import axios from "axios";
 
 const PostsPage = () => {
   const [posts, setPosts] = useState([]);
@@ -14,49 +15,19 @@ const PostsPage = () => {
   };
 
   useEffect(() => {
-    setPosts([
-      {
-        _id: "1",
-        title: "Freelance Web Development Project",
-        description:
-          "Looking for a freelance web developer to build a responsive website for an e-commerce business.",
-        taskTime: "2 weeks",
-        taskPayment: "$1500",
-        workType: "remote",
-        posterId: "user1",
-        applicants: ["user2", "user3"],
-        status: "open",
-        dateCreated: new Date(),
-      },
-      {
-        _id: "2",
-        title: "Graphic Design for Social Media Campaign",
-        description:
-          "Need a graphic designer to create engaging visuals for a social media marketing campaign.",
-        taskTime: "1 week",
-        taskPayment: "$500",
-        workType: "remote",
-        posterId: "user2",
-        applicants: ["user1", "user3"],
-        status: "open",
-        dateCreated: new Date(),
-      },
-    ]);
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/posts/all");
+        console.log(response);
+        const data = await response.data;
+        setPosts(data.posts);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-    // const fetchPosts = async () => {
-    //   try {
-    //     // const response = await fetch('/api/posts');
-    //     if (!response.ok) {
-    //       throw new Error('Failed to fetch posts');
-    //     }
-    //     const data = await response.json();
-    //     setPosts(data.posts);
-    //   } catch (error) {
-    //     console.error(error);
-    //   }
-    // };
+    fetchPosts();
 
-    // fetchPosts();
   }, []);
 
   return (
