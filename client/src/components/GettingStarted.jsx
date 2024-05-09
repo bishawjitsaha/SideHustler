1133ca57bf6c79c2f0a2ab99dafa4f6224eeb2e0
bishawjitsaha <87948050+bishawjitsaha@users.noreply.github.com>
2024-05-08 21:22:36 -0400

    Update GettingStarted.jsx

--- client/src/components/GettingStarted.jsx
+++ client/src/components/GettingStarted.jsx
@@ -58,6 +58,7 @@ function GettingStarted(){
   return(
     <div className='flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8'>
       <div className="max-w-md w-full space-y-8">
+        <h1 className="mt-2 text-center text-4xl font-bold text-gray-900">Getting Started</h1>
         <form onSubmit={handleSubmit} className='mt-8 space-y-6'>
           <div className='rounded-md shadow-sm -space-y-px'>
             <div>
