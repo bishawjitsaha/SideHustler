8de3dd24af1027fb6ae70f05dbaaf112c501b726
bishawjitsaha <87948050+bishawjitsaha@users.noreply.github.com>
2024-05-08 21:22:04 -0400

    Update GettingStarted.jsx

--- client/src/components/GettingStarted.jsx
+++ client/src/components/GettingStarted.jsx
@@ -119,7 +119,7 @@ function GettingStarted(){
           </div>
           <br/>
           <button
-            className='button'
+            className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-4'
             id='submitButton'
             name='submitButton'
             type='submit'
